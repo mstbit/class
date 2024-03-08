d32008cbe9924942651aed3b82aac89ae8ae13f0
tychen742 <tychen742@gmail.com>
2024-03-08 09:59:15 -0600

    again

--- 11_list_methods_a.py
+++ 11_list_methods_a.py
@@ -3,11 +3,11 @@ fruits = ['apple', 'banana', 'cherry']
 print("0. list", fruits)
 
 # 1. append()
-fruits.append('')
+fruits.append('durian')
 print("1. append: ", fruits)   # 1. ['apple', 'banana', 'cherry', 'durian']
 
 # 2. insert()
-fruits.insert()
+fruits.insert(3, 'dragonfruit')
 print("2. insert: ", fruits)
 
 # 3. extend()
@@ -20,11 +20,11 @@ print("4. index: ", fruits.index("banana"))     #4. index: 1
 
 nums = [1, 2, 3, 4, 9, 2, 2, 5, 6, 7]
 # print("index(): ", nums.index(2, start=4, end=9))
-print("4. index: ", nums.index())        #4. index: 5
+print("4. index: ", nums.index(2, ))        #4. index: 5
 
 # 5. count()
 fruits.append("cherry")
-print("5. count: ", fruits.count('cherry'))
+print("5. count: ", fruits.count(''))
 
 # 6. revers(0)
 fruits.reverse()
