1900c39f13e40c4ba68eb12ff77688051c655891
Tsangyao Chen <tychen742@gmail.com>
2024-03-19 18:03:24 -0500

    "function aaa updated"

---
+++ test.py
@@ -0,0 +1,16 @@
+import turtle as t
+import time
+t.color('red')
+t.fillcolor('yellow')
+t.begin_fill()
+
+while True:
+    t.forward(200)
+    t.left(170)
+    if abs(t.pos()) < 1:
+        break
+
+t.end_fill()
+
+time.sleep(5)
+t.mainloop()
