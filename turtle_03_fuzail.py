d7731c5fe8006b43dc4c973ccbeda27fff3ba937
fuzail97 <fuzailsyed.19971@gmail.com>
2024-03-22 10:27:50 -0500

    commiting

--- turtle_03_fuzail.py
+++ turtle_03_fuzail.py
@@ -1,14 +1,46 @@
 import turtle as t
 
+# def sunflower(color, length, degree, fillcolor='grey', bgcolor='purple'):
+    
+#     t.bgcolor(bgcolor)
+#     t.color(color)
+#     t.fillcolor(fillcolor)
+#     t.begin_fill()
 
-while True:
+
+
+#     while True:
     
-    t.color('red')
-    t.forward(200)
-    t.left(170)
-    if abs(t.pos()) <1:
-        break
+#         t.forward(length)
+#         t.left(degree)
+#         if abs(t.pos()) <1:
+#             break
     
+#     time.sleep(5)
+#     t.clearscreen()
+#     t.mainloop()
+    
+    
+# sunflower('red', 200, 170)
+
+#square
+
+# t.bgcolor('orange')
+# t.shape('turtle')
+
+# for i in range(4):
+#     t.color('purple')
+#     t.forward(100)
+#     t.right(90)
+    
+#circle
+
+# t.teleport(100, 100)
+# t.circle(100)
+
+# star
+
+t.teleport(-200, -200)
+t.right(75)
+t.forward(100)
 time.sleep(5)
-t.clearscreen()
-t.mainloop()
