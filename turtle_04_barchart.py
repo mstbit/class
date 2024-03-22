0687d33fd4f4f70ad05083f819f72facfa741255
tychen742 <tychen742@gmail.com>
2024-03-22 09:53:35 -0500

    updates

--- turtle_04_barchart.py
+++ turtle_04_barchart.py
@@ -1,8 +1,9 @@
 import turtle as t
-
+import calc
 
 t.bgcolor('orange')
 t.shape('turtle')
+# t.speed(250)
 
 
 def drawBar(t, height):
@@ -15,62 +16,27 @@ def drawBar(t, height):
     t.forward(height)        # And down again!
     t.left(90)               # put the turtle facing the way we found it.
 
-xs = [48, 117, 200, 240, 160, 260, 220]
 
-for v in xs:                 # assume xs and tess are ready
+
+# xs = [48, 117, 200, 240, 160, 260, 220]
+
+# for v in xs:                 
+#     drawBarA(t, v)
+
+
+
+# Driver Code
+
+
+a_list = [1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5]
+a_dic = (calc.freq(a_list))
+# print(a_dic)
+lst = list(a_dic.values())
+# print(lst)
+for v in lst:
     drawBar(t, v)
 
 
-
-# Function that draws the turtle
-def drawBar(t, height, color):
-   
-    # Get turtle t to draw one bar
-    # of height
-      
-    # Start filling this shape
-    t.fillcolor(color)
-    t.begin_fill()              
-    t.left(90)
-    t.forward(height)
-    t.write(str(height))
-    t.right(90)
-    t.forward(40)
-    t.right(90)
-    t.forward(height)
-    t.left(90)
-      
-    # stop filling the shape
-    t.end_fill()                 
- 
-# Driver Code
- 
-xs = [48, 117, 200, 96, 134, 260, 99]
-clrs = ["green", "red", "yellow", "black",
-        "pink", "brown", "blue"]
- 
-maxheight = max(xs)
-numbers = len(xs)
-border = 10
-  
-# Set up the window and its
-# attributes
-wn = t.Screen()             
-wn.setworldcoordinates(0 - border, 0 - border, 
-                       40 * numbers + border,
-                       maxheight + border)
-  
-# Create tess and set some attributes
-tess = t.Turtle()           
-tess.pensize(3)
-  
-for i in range(len(xs)):
-     
-    drawBar (tess, xs[i],
-             clrs[i])
- 
-wn.exitonclick()
-
-### housekeeping
-t.exitonclick()
+# housekeeping
+# t.exitonclick()
 t.mainloop()
