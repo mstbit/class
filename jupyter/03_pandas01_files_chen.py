973075c462bd5b1e5821190300fa35c79d45529a
tychen742 <tychen742@gmail.com>
2024-04-09 12:15:42 -0500

    commit @ 2024-0409-121542

--- jupyter/03_pandas01_files_chen.py
+++ jupyter/03_pandas01_files_chen.py
@@ -8,18 +8,18 @@ print("HOMMMMMMMMMMM", HOME)
 data_path = HOME + '/workspace/class/jupyter/data'
 print("WWWWWWWWWWWWW", data_path)
 
-# ##### option 1:  DOES NOT WORK in Jupyter #####
-# df = pd.read_csv("data/data.csv")
+# ##### option 1:  ##### works in mac 
+# df = pd.read_csv(data_path+"/data.csv")
 # df.close()
 
-# ##### option 2:  DOES NOT WORK in Jupyter #####
-with open(data_path+"/data.csv") as f:
-    df = pd.read_csv(f)
+# ##### option 2:  ##### works in mac 
+# with open(data_path+"/data.csv") as f:
+#     df = pd.read_csv(f)
 
-# ##### option 3: path.join #####
-data = path.join(data_path, "data.csv")
-with open(data) as f:
-    df = pd.read_csv(f)
+# ##### option 3: path.join ##### works in mac
+# data = path.join(data_path, "data.csv")
+# with open(data) as f:
+#     df = pd.read_csv(f)
 
 ##### option 4: change dir #####
 # ##### option 3: path.join ##### works in Windows 11/macOS
@@ -27,7 +27,7 @@ with open(data) as f:
 # with open(data) as f:
 #     df = pd.read_csv(f)
 
-##### option 4: change dir ##### works in Windows 11/macOS
+##### option 5: change dir ##### works in Windows 11/macOS
 # os.chdir(data_path)
 # df = pd.read_csv("data.csv")
 
