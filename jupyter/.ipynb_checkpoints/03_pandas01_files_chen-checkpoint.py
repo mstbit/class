6b11aecdfe309da0e670eeb6da6487f4b9cdca93
tychen742 <tychen742@gmail.com>
2024-04-09 13:03:08 -0500

    commit @ 2024-0409-130308

--- jupyter/.ipynb_checkpoints/03_pandas01_files_chen-checkpoint.py
+++ jupyter/.ipynb_checkpoints/03_pandas01_files_chen-checkpoint.py
@@ -8,28 +8,28 @@ print("HOMMMMMMMMMMM", HOME)
 data_path = HOME + '/workspace/class/jupyter/data'
 print("WWWWWWWWWWWWW", data_path)
 
-# ##### option 1: ##### works in macOS/Windows/Jupyter
+# ##### option 1: ##### works in macOS/Windows
 # df = pd.read_csv(data_path+"/data.csv")
 
 
-# ##### option 2: with open ##### works in macOS/Windows/Jupyter
-with open(data_path+"/data.csv") as f:
-    df = pd.read_csv(f)
+# ##### option 2: with open ##### works in macOS/Windows
+# with open(data_path+"/data.csv") as f:
+#     df = pd.read_csv(f)
 
-# ##### option 3: path.join ##### works in mac/Windows/Jupyter
+# ##### option 3: path.join ##### works in mac/Windows
 # data = path.join(data_path, "data.csv")
 # with open(data) as f:
 #     df = pd.read_csv(f)
 
-# ##### option 4: path.join ##### works in Windows/macOS/Jupyter
+# ##### option 4: path.join ##### works in Windows/macOS
 # data = path.join(data_path, "data.csv")
 # with open(data) as f:
 #     df = pd.read_csv(f)
 
-##### option 5: change dir ##### works in Windows 11/macOS/Jupyter
+##### option 5: change dir ##### works in Windows 11/macOS
 # os.chdir(data_path)
 # df = pd.read_csv("data.csv")
 
 # ##### output
-df.describe()
+print(df.describe())
 
